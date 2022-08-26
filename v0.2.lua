3506c42e56c6359c7926ccb68787fca56f9f17f1
Relkss <86829404+Relkss@users.noreply.github.com>
2022-08-26 16:41:52 +0800

    Update v0.2.lua

--- v0.2.lua
+++ v0.2.lua
@@ -1,11 +1,9 @@
-getgenv().Settings = {
-	Keybind = Enum.KeyCode.H
-	SkipRoom50 = true -- default is true
-	AutoUnlockDoors = true -- default is true
-}
+local Keybind = Enum.KeyCode.H
+local CanSkipRoom_50 = true
+local CanAutoUnlockDoors = true
 
 do
-	local UIS = game:GetService("UserInputService")
+local UIS = game:GetService("UserInputService")
 local CF = CFrame.new
 local LatestRoom = game:GetService("ReplicatedStorage").GameData.LatestRoom
 
@@ -13,35 +11,31 @@ UIS.InputBegan:Connect(function(input, typing)
     if typing then return end
     if input.KeyCode == Keybind then
         pcall(function()
-				local key = false
-            	local CurrentDoor = workspace.CurrentRooms[tostring(game:GetService("ReplicatedStorage").GameData.LatestRoom.Value)]:WaitForChild("Door")
-				for i,object in ipairs(CurrentDoor.Parent:GetDescendants()) do
-                    	if object.Name == "KeyObtain" then
-                        	key = object
-                    	end
-                	end
-            	if LatestRoom.Value == 50 and Settings.SkipRoom50 == true then
-					if Settings.SkipRoom50 == true then
-						CurrentDoor = workspace.CurrentRooms[tostring(LatestRoom.Value + 1)]:WaitForChild("Door")
-                		game.Players.LocalPlayer.Character:PivotTo(CF(CurrentDoor.Door.Position))
-					end
-            	else
-					if key and Settings.AutoUnlockDoors == true then
-						if Settings.AutoUnlockDoors == true then
-							game.Players.LocalPlayer.Character:PivotTo(CF(key.Hitbox.Position))
-						task.wait(.3)
-							fireproximityprompt(key.ModulePrompt)
-						task.wait(.3)
-							game.Players.LocalPlayer.Character:PivotTo(CF(CurrentDoor.Door.Position))
-						task.wait(.4)
-							fireproximityprompt(CurrentDoor.Lock.UnlockPrompt)
-						end
-					end
-                	game.Players.LocalPlayer.Character:PivotTo(CF(CurrentDoor.Door.Position))
-            	end
-				task.wait(.2)
-				CurrentDoor.ClientOpen:FireServer()
-        	end)
-    	end
-	end)
+            local key = false
+            local CurrentDoor = workspace.CurrentRooms[tostring(game:GetService("ReplicatedStorage").GameData.LatestRoom.Value)]:WaitForChild("Door")
+            for i,object in ipairs(CurrentDoor.Parent:GetDescendants()) do
+                if object.Name == "KeyObtain" then
+                    key = object
+                end
+            end
+            if LatestRoom.Value == 50 and CanSkipRoom_50 == true then
+                CurrentDoor = workspace.CurrentRooms[tostring(LatestRoom.Value + 1)]:WaitForChild("Door")
+                game.Players.LocalPlayer.Character:PivotTo(CF(CurrentDoor.Door.Position))
+            else
+                if key and CanAutoUnlockDoors == true then
+                    game.Players.LocalPlayer.Character:PivotTo(CF(key.Hitbox.Position))
+                    task.wait(.3)
+                    fireproximityprompt(key.ModulePrompt)
+                    task.wait(.3)
+                    game.Players.LocalPlayer.Character:PivotTo(CF(CurrentDoor.Door.Position))
+					task.wait(.3)
+                    fireproximityprompt(CurrentDoor.Lock.UnlockPrompt)
+                end
+                game.Players.LocalPlayer.Character:PivotTo(CF(CurrentDoor.Door.Position))
+            end
+            task.wait(.5)
+            CurrentDoor.ClientOpen:FireServer()
+        end)
+    end
+end)
 end
